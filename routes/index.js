18b1bbe2303fa5eebe375f5dabb805c41e9452c5
UmG18 <umang18oct@gmail.com>
2017-03-11 05:37:53 +0530

    3 basic page layouts made

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home - desiCinephiles' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us - desiCinephiles' });
+});
+router.get('/movies', function(req, res, next) {
+  res.render('movie', { title: 'Movies - desiCinephiles' });
 });
 
 module.exports = router;
